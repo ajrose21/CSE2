542a3ea88b9150a7343c29bb438c22baa463a860
ajrose21 <ajr217@lehigh.edu>
2014-08-31 14:51:41 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////
+//Abi Rose
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+//  add main method
+        public static void main(String [] args) {
+            
+//  Print the statement below
+        System.out.println("  ----------- ");
+        System.out.println("  | WELCOME | ");
+        System.out.println("  -----------");
+        System.out.println("  ^ ^ ^ ^ ^ ^");
+        System.out.println(" /\\ /\\ /\\ /\\ /\\ ");
+        System.out.println("|-A--J--R--2--1--7-|");
+        System.out.println(" \\/ \\/ \\/ \\/ \\/ ");
+        System.out.println("  v v v v v v");
+        System.out.println("This summer my family got two kittens and we named them Daisy and Dante.");
+        System.out.println("test");
+        
+        //test/testtest
+        }
+}
