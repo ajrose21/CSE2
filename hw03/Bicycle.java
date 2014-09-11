896549dd1be0b9cc96522788fe8f1a817cc200f5
ajrose21 <ajr217@lehigh.edu>
2014-09-11 22:12:03 +0000

    updated BigMac

---
+++ hw03/Bicycle.java
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////
+//Abi Rose
+//hw03
+//Bicycle Java Program
+//
+//  Program Should:
+//      compute and print distance traveled
+//      compute and print average miles per hour
+//
+//  first compile the program
+//      javac.Bicycle.java
+//  run the program
+//      java Bicycle//
+
+//define a class
+public class Bicycle{
+    
+//  add main method
+        public static void main(Strin [] args) {
+            
+        //defining variables
+        
+        int secsTrip=500;
+        int countsTrip=1500;
+        double wheelDiameter=27.0;
+        PI=3.14159,
+        feetPerMile=5280,
+        inchesPerFoot=13,
+        secondsPerMinute=60;
+        double distanceTrip;
+        
+        
+        
+//Print the statement below
+        }
+}
